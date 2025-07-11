5fe4ddb0cffc23e9f63e83630c33635627ec3e71
JonatanFD <u20211b995@upc.edu.pe>
2025-07-11 17:13:47 -0500

    Update Redis environment variables; streamline Redis connection setup; use .env in docker-compose; remove unused JWT middleware

--- env.py
+++ env.py
@@ -1,10 +1,10 @@
+import os
 import dotenv
 from redis_om import get_redis_connection
 
 ENV_PATH = ".env"
+dotenv.load_dotenv(ENV_PATH)
 
-REDIS_URL = dotenv.get_key(dotenv_path=ENV_PATH, key_to_get="REDIS_URL")
-print("REDIS_URL:", REDIS_URL)
-
-
-REDIS_CONN = get_redis_connection(url=REDIS_URL, decode_responses=True, )
+# Usar la URL completa de REDIS_URL del .env
+REDIS_URL = os.getenv("REDIS_URL")
+REDIS_CONN = get_redis_connection(url=REDIS_URL, decode_responses=True)
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from contextlib import asynccontextmanager
 from http.client import HTTPException
 
-from fastapi import FastAPI, Request
+from dotenv import load_dotenv
+from fastapi import FastAPI
 from fastapi.routing import APIRoute
-from redis_om import Migrator
+from redis_om import Migrator, get_redis_connection
 
 from controllers import auth, companies, posts
 from env import REDIS_URL
 from services.jwt import verify_access_token
+
+load_dotenv(verbose=True)
+
+
+REDIS_CONN = get_redis_connection(url=REDIS_URL, decode_responses=True)
 
 
 @asynccontextmanager
@@ -21,22 +27,6 @@ app.include_router(auth.router)
 app.include_router(companies.router)
 app.include_router(posts.router)
 
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    bearer_token = request.headers.get("Authorization")
-    # path
-    print(request.base_url)
-
-    if bearer_token and bearer_token.startswith("Bearer "):
-        token = bearer_token.split("Bearer ")[1]
-        try:
-            verify_access_token(token)
-        except HTTPException:
-            return HTTPException(status_code=401, detail="Unauthorized")
-
-    response = await call_next(request)
-    return response
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -47,30 +37,9 @@ async def health_check():
     """
     Health check endpoint to verify the service is running.
     """
-    import redis
-    r = redis.Redis.from_url(REDIS_URL)
-    print(r.ping())  # → Debería dar True
-    print(r.execute_command("command", "info", "json.set"))
+    print("Redis url", REDIS_URL)
+    print(REDIS_CONN.ping())  # → Debería dar True
+    print(REDIS_CONN.execute_command("command", "info", "json.set"))
 
     return {"status": "ok", "message": "Service is running"}
 
-def jwt_protected_routes(app: FastAPI):
-    for route in app.routes:
-        if isinstance(route, APIRoute):
-            if route.path not in ["/auth/sign-in", "/auth/sign-up"]:
-                original_endpoint = route.endpoint
-
-                async def protected_endpoint(request: Request, *args, **kwargs):
-                    bearer_token = request.headers.get("Authorization")
-                    if not bearer_token or not bearer_token.startswith("Bearer "):
-                        raise HTTPException(status_code=401, detail="Unauthorized")
-                    token = bearer_token.split("Bearer ")[1]
-                    try:
-                        verify_access_token(token)
-                    except Exception:
-                        raise HTTPException(status_code=401, detail="Invalid or expired token")
-                    return await original_endpoint(*args, **kwargs)
-
-                route.endpoint = protected_endpoint
-
-jwt_protected_routes(app)
